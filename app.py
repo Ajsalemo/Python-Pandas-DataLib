c39f8f33c98fcd36b78d3e4442cac04eeb395501
Anthony Salemo <ajssalemo@gmail.com>
2021-02-04 20:33:08 -0500

    added a pandas dataframe to a basic flask endpoint

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+import pandas as pd
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    sample_dict = {
+        "Name": [
+                "Braund, Mr. Owen Harris",
+                "Allen, Mr. William Henry",
+                "Bonnell, Miss. Elizabeth",
+            ],
+            "Age": [22, 35, 58],
+            "Sex": ["male", "male", "female"],
+    }
+
+    df = pd.DataFrame(sample_dict)
+    print(df)
+    return df.to_json()
