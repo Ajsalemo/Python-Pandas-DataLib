f969b8c3255e22b4ac2c34cc8b6d002eecdc5ab1
Anthony Salemo <ajssalemo@gmail.com>
2021-02-04 23:46:10 -0500

    added a way to filter by location name in the DataFrame including ignoring empty values

--- app.py
+++ app.py
@@ -7,19 +7,17 @@ app = Flask(__name__)
 number_stations = pd.read_csv("./data/number_stations.csv")
 
 
+# List all stations
 @app.route('/')
 def index():
-    df = pd.DataFrame(number_stations)
-    print(df)
-    f = df.to_json()
+    f = number_stations.to_json()
     return json.loads(f)
 
 
 @app.route('/station_names')
 def station_names():
-    st_df = pd.DataFrame(number_stations)
-    # Filter the DataFrame columns to retrieve all stations under 'Number Station name'
-    st_name = st_df['Name']
+    # Filter the DataFrame columns to retrieve all stations under 'Name' column
+    st_name = number_stations['Name']
     # If the DataFrame returns a response, convert the Series to a JSON string
     f = st_name.to_json()
     # Then convert the JSON string to a dict to properly format it when sending the response to the client
@@ -52,5 +50,36 @@ def filer_station_names(name):
         return jsonify({ 'error': 'That station does not exist'})
     # If the DataFrame returns a response, convert the Series to a JSON string
     r = f_name.to_json()
+    # Then convert the JSON string to a dict to properly format it when sending the response to the client
+    return json.loads(r)
+
+
+@app.route('/filter_by_id/<id>')
+def filter_by_id(id):
+    if id is None:
+        return jsonify({ 'error': 'ID cannot be empty' })
+    # Pass the parameter from the URL and check if it matches against a known station id
+    f_id = number_stations[number_stations['ID'] == id]
+    # If the DataFrame returns empty, send an error back to the client
+    if f_id.empty:
+        return jsonify({ 'error': 'That ID does not exist'})
+    # If the DataFrame returns a response, convert the Series to a JSON string
+    r = f_id.to_json()
+    # Then convert the JSON string to a dict to properly format it when sending the response to the client
+    return json.loads(r)
+
+
+@app.route('/filter_by_location/<loc>')
+def filter_by_location(loc):
+    print(loc)
+    if loc is None:
+        return jsonify({ 'error': 'Location cannot be empty' })
+    # Pass the parameter from the URL and check if it matches against a known location - 'na=False' ignores any possibly empty values in the column to avoid throwing an error
+    f_loc = number_stations[number_stations['Location'].str.contains(loc, na=False)]
+    # If the DataFrame returns empty, send an error back to the client
+    if f_loc.empty:
+        return jsonify({ 'error': 'That location does not exist'})
+    # If the DataFrame returns a response, convert the Series to a JSON string
+    r = f_loc.to_json()
     # Then convert the JSON string to a dict to properly format it when sending the response to the client
     return json.loads(r)
